cd2e386b7ad6ff9a14d88c72aa4f98f1f9bf8ff5
chejito <smndz81@gmail.com>
2022-02-02 15:05:09 +0100

    README añadido

---
+++ README.md
@@ -0,0 +1,7 @@
+# API Rest Procadec
+## Proyecto Final CFGS DAM Ilerna
+
+* Java 11
+* Spring Boot 2
+* PostgreSQL
+
--- src/main/java/com/procadec/app/repository/ProductoRepository.java
+++ src/main/java/com/procadec/app/repository/ProductoRepository.java
@@ -1,13 +1,9 @@
 package com.procadec.app.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.procadec.app.entity.Producto;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-		
-}
+public interface ProductoRepository extends JpaRepository<Producto, Long> {}
